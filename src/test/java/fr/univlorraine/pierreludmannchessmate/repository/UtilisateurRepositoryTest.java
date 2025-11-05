c72f3137e65ddb4286f80ecd5946aeb707dfb724
timothe-tln <e70959u@etu.univ-lorraine.fr>
2025-11-05 17:26:12 +0100

    Ajustement des commentaires + suppression dépendance qui sert à rien

--- src/test/java/fr/univlorraine/pierreludmannchessmate/repository/UtilisateurRepositoryTest.java
+++ src/test/java/fr/univlorraine/pierreludmannchessmate/repository/UtilisateurRepositoryTest.java
@@ -3,7 +3,7 @@ package fr.univlorraine.pierreludmannchessmate.repository;
 import fr.univlorraine.pierreludmannchessmate.model.Utilisateur;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
