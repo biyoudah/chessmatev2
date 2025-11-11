f3970387cc3d506fd8f1aae6256a5cefd4ffad04
timothe-tln <e70959u@etu.univ-lorraine.fr>
2025-11-11 16:07:34 +0100

    correction après merge

--- src/main/java/fr/univlorraine/pierreludmannchessmate/ChessController.java
+++ src/main/java/fr/univlorraine/pierreludmannchessmate/ChessController.java
@@ -3,7 +3,12 @@ package fr.univlorraine.pierreludmannchessmate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
