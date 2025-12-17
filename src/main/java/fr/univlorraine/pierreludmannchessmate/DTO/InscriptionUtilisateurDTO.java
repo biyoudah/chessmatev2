5e9883b5828cb335abbb0e92d8e554abf4b7d730
charlotte-o3o <charlotte.diloreto.cdl@gmail.com>
2025-12-17 17:31:08 +0100

    css

--- src/main/java/fr/univlorraine/pierreludmannchessmate/DTO/InscriptionUtilisateurDTO.java
+++ src/main/java/fr/univlorraine/pierreludmannchessmate/DTO/InscriptionUtilisateurDTO.java
@@ -9,22 +9,22 @@ import jakarta.validation.constraints.Size;
 public class InscriptionUtilisateurDTO {
 
     // Contrainte pour l'e-mail : ne doit pas être vide et doit suivre un format d'e-mail valide
-    @Email(message = "Format d'e-mail invalide")
-    @NotBlank(message = "L'e-mail est obligatoire")
-    @Size(max = 100, message = "L'e-mail ne peut dépasser 100 caractères")
+    @Email(message = "Format d'e-mail invalide.")
+    @NotBlank(message = "L'e-mail est obligatoire.")
+    @Size(max = 100, message = "L'e-mail ne peut dépasser 100 caractères.")
     private String email;
 
     // Contrainte pour le nom d'utilisateur : obligatoire, entre 3 et 50 caractères
     // Et doit correspondre à une expression régulière (ici, seulement des lettres et chiffres)
-    @NotBlank(message = "Le pseudo est obligatoire")
-    @Size(min = 3, max = 30, message = "Le pseudo doit contenir entre 3 et 30 caractères")
-    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Le pseudo ne doit contenir que des lettres et des chiffres")
+    @NotBlank(message = "Le pseudo est obligatoire.")
+    @Size(min = 3, max = 30, message = "Le pseudo doit contenir entre 3 et 30 caractères.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Le pseudo ne doit contenir que des lettres et des chiffres.")
     private String pseudo;
 
     // Contrainte pour le mot de passe : obligatoire, entre 8 et 30 caractères,
     // et doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial.
-    @NotBlank(message = "Le mot de passe est obligatoire")
-    @Size(min = 8, max = 30, message = "Le mot de passe doit contenir entre 8 et 30 caractères")
+    @NotBlank(message = "Le mot de passe est obligatoire.")
+    @Size(min = 8, max = 30, message = "Le mot de passe doit contenir entre 8 et 30 caractères.")
     @Pattern(
             regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9\\s]).{8,30}$",
             message = "Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial."
